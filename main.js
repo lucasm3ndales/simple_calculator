c41e38f9d122308414df84f67a858353c1a5a95f
Lucas Mendes <lucasm3ndales@gmail.com>
2022-02-19 00:03:50 -0300

    simple-calculator-1.0

---
+++ main.js
@@ -0,0 +1,27 @@
+function insert(num)
+{
+    let numero = document.getElementById('result').innerHTML;
+    document.getElementById('result').innerHTML = numero + num;
+
+}
+
+function clean()
+{
+    document.getElementById('result').innerHTML=""
+}
+
+function back()
+{
+    let result=document.getElementById('result').innerHTML;
+    document.getElementById('result').innerHTML = result.substring(0, result.length -1)
+}
+
+function calcular()
+{
+    let result=document.getElementById('result').innerHTML;
+    if('result')
+    {
+        document.getElementById('result').innerHTML= eval(result)
+
+    }
+}    
